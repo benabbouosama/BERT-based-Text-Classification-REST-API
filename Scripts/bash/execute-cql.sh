1109574a92ce3faa2649dda30f173dd65af4926c
oussamakyu <oussamakun2002@gmail.com>
2024-04-21 06:01:28 +0100

    Added initialization script for Cassandra and schema creation , and scripts to create keyspace and tables.

---
+++ Scripts/bash/execute-cql.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "========================Starting to execute SH script...========================"
+USER_NAME='cassandra'
+PASSWORD='cassandra'
+
+while ! cqlsh cassandra_db -u "${USER_NAME}" -p "${PASSWORD}" -e 'describe cluster' ; do
+     echo "========================Waiting for main instance to be ready...========================"
+     sleep 1
+done
+
+for cql_file in ./tmp/cql/*.cql;
+do
+  cqlsh cassandra_db -u "${USER_NAME}" -p "${PASSWORD}" -f "${cql_file}" ;
+  echo "========================Script ""${cql_file}"" executed!!!========================"
+done
+echo "========================Execution of SH script is finished!========================"
+echo "========================Stopping temporary instance!========================"
